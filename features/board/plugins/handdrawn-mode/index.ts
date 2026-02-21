9057ecaf4ae6a37834e447d1e2114f2271c1f632
kripu77 <khadkakripu4@gmail.com>
2026-02-21 18:46:03 +1100

    Add handdrawn mode exports and types for configuration and presets

---
+++ features/board/plugins/handdrawn-mode/index.ts
@@ -0,0 +1,12 @@
+export { 
+  isHanddrawn, 
+  setHanddrawn, 
+  withHanddrawn, 
+  BOARD_TO_HANDDRAWN,
+  BOARD_TO_HANDDRAWN_CONFIG,
+  getHanddrawnConfig,
+  HANDDRAWN_PRESETS,
+  DEFAULT_HANDDRAWN_CONFIG,
+} from './with-handdrawn';
+export { toRoughOptions } from './config';
+export type { HanddrawnConfig, HanddrawnPreset } from './with-handdrawn';
