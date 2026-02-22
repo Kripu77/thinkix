fecb0c4efd56cd8577ef5467148bea73d7244d5b
kripu77 <khadkakripu4@gmail.com>
2026-02-22 12:24:59 +1100

    Add AppMenu export to toolbar index

--- features/toolbar/index.ts
+++ features/toolbar/index.ts
@@ -1,3 +1,4 @@
 export { BoardToolbar } from './components/BoardToolbar';
 export { ZoomToolbar } from './components/ZoomToolbar';
+export { AppMenu } from './components/AppMenu';
 export * from './components/inline';
