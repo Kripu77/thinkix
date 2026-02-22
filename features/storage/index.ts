4a1dc7981e86c5e5614daaa90515247e9d579040
kripu77 <khadkakripu4@gmail.com>
2026-02-22 12:24:48 +1100

    export

--- features/storage/index.ts
+++ features/storage/index.ts
@@ -1,12 +1,4 @@
-// Hooks
 export { useAutoSave } from './hooks/use-auto-save';
-
-// Store (re-export from package)
 export { useBoardStore } from '@thinkix/storage';
-
-// Components
 export { BoardSwitcher } from './components/BoardSwitcher';
-export { SaveIndicator } from './components/SaveIndicator';
-
-// Types (re-export from package)
 export type { Board, BoardMetadata, SaveStatus } from '@thinkix/storage';
