08957cf63f39d978365ca3f75842293349209f50
kripu77 <khadkakripu4@gmail.com>
2026-02-23 21:06:22 +1100

    Add parseMarkdownToMindElement export in plait-utils
    This commit introduces the export of the `parseMarkdownToMindElement` function from the markdown-parser module, enhancing the utility of the plait-utils package by enabling markdown parsing capabilities for mind map elements.

--- packages/plait-utils/lib/index.ts
+++ packages/plait-utils/lib/index.ts
@@ -4,3 +4,4 @@ export type { TextMarks } from './text-transforms';
 export { getTextAccessor } from './text-accessor';
 export type { TextAccessor } from './text-accessor';
 export { asPlaitPlugin } from './plugin-utils';
+export { parseMarkdownToMindElement } from './markdown-parser';
