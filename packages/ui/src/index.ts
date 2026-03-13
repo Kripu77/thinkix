f102c6e167cc6756c3ca7a61460fde02beea58e8
kripu77 <khadkakripu4@gmail.com>
2026-03-13 21:42:50 +1100

    Add Select component from radix-ui

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -4,6 +4,7 @@ export * from './components/ui/separator';
 export * from './components/ui/toggle';
 export * from './components/ui/toggle-group';
 export * from './components/ui/dropdown-menu';
+export * from './components/ui/select';
 export * from './components/ui/image-viewer';
 export * from './components/ui/dialog';
 export * from './components/ui/input';
